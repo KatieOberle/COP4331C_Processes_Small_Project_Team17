ffac7b51f133399d070f601a576415704df5a154
scrubbyJoe <paytonheckman@gmail.com>
2026-09-16 14:44:34 -0400

    Update $conn statements to match final database implementation

--- payton_files/small_project/Login.php
+++ payton_files/small_project/Login.php
@@ -7,7 +7,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "small_project_test"); 	
+	$conn = new mysqli("localhost", "contactapp", "YourChosenPassword1!", "ContactManager"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );

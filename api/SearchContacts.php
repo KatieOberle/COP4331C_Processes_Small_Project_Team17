a318308917700f3ee3c2ccbeb4e7bf88d7379077
scrubbyJoe <paytonheckman@gmail.com>
2026-09-22 15:24:42 -0400

    Put back removed function definitions in SearchContact.php

--- api/SearchContacts.php
+++ api/SearchContacts.php
@@ -68,4 +68,21 @@ session_set_cookie_params([
 		$stmt->close();
 		$conn->close();
 	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"results":[],"error": ' . $err . '}';
+		sendResultInfoAsJson( $retValue );
+	}
 ?>
